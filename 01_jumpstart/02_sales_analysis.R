6bcdd9df5c73a16f0bf296fac3caec8b1be5e552
Steven Sanderson <spsanderson@gmail.com>
2019-10-07 14:48:42 -0400

    first sales analysis

---
+++ 01_jumpstart/02_sales_analysis.R
@@ -0,0 +1,78 @@
+# DS4B 101-R: R FOR BUSINESS ANALYSIS ----
+# JUMPSTART: First Sales Analysis ----
+
+# 1.0 Load libraries ----
+if(!require(pacman)) install.packages("pacman")
+pacman::p_load(
+    # Work horse packages
+    "tidyverse",
+    "lubridate",
+    
+    # theme_tq()
+    "tidyquant",
+    
+    # Excel Files
+    "readxl",
+    "writexl"    
+)
+
+# 2.0 Importing Files ----
+
+
+
+
+
+# 3.0 Examining Data ----
+
+
+
+
+
+# 4.0 Joining Data ----
+
+
+
+
+# 5.0 Wrangling Data ----
+
+
+
+
+
+# 6.0 Business Insights ----
+
+
+# 6.1 Sales by Year ----
+
+# Step 1 - Manipulate
+
+
+
+
+# Step 2 - Visualize
+
+
+
+# 6.2 Sales by Year and Category 2 ----
+
+
+# Step 1 - Manipulate
+
+
+
+
+# Step 2 - Visualize
+
+
+
+
+# 7.0 Writing Files ----
+
+
+# 7.1 Excel ----
+
+
+# 7.2 CSV ----
+
+
+# 7.3 RDS ----
