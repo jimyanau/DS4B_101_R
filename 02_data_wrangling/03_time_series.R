e80a6fc968de50d5709a17f96240c7bd6fc9ccba
Steven Sanderson <spsanderson@gmail.com>
2019-10-29 15:16:36 -0400

    time_series wrangling

--- 02_data_wrangling/03_time_series.R
+++ 02_data_wrangling/03_time_series.R
@@ -13,20 +13,28 @@ glimpse(bike_orderlines_tbl)
 
 # 1.0 Date & Lubridate Basics ----
 # 1.1 Character vs Date/Datetime
+order_date_tbl <- bike_orderlines_tbl %>%
+    select(order_date)
 
+order_date_tbl %>%
+    pull(order_date) %>%
+    class()
 
 # 1.2 Date Classes
-
-
-
-
+order_date_tbl %>%
+    mutate(order_date_char = as.character(order_date)) %>%
+    mutate(order_date_char2 = order_date_char %>% str_c(" 00:00:00")) %>%
+    
+    # ymd
+    mutate(order_date_date = order_date_char %>% ymd()) %>%
+    mutate(order_date_dttm = order_date_char2 %>% ymd_hms())
 
 
 # 1.3 Lubridate Functions
-
 # Conversion
-
-
+"06/01/18" %>% mdy() %>% class()
+"06/01/18 12:30:15" %>% mdy_hms()
+"January 1, 1985" %>% mdy()
 
 # Extractor
 
